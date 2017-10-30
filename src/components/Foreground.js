4b06722508bc050c0d5f731f771bf2d90fddff6c
Developer 787 <nodejs787@gmail.com>
2017-10-30 15:32:53 -0400

    semantic buttons

--- src/components/Foreground.js
+++ src/components/Foreground.js
@@ -1,6 +1,25 @@
 import React from 'react'
-
+import { Button, Input, Header, Container } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 import './Foreground.css'
+const buttons = () => (
+	<div className="buttons">
+	<Button 
+		className="button-ui"
+	  color="green"
+	  circular 
+	  size="massive" 
+	  icon="arrow left">
+	</Button>
+	<Button 
+		className="button-ui"
+	  color="green"
+	  size="massive" 
+	  circular 
+	icon="arrow right">
+	</Button>
+	</div>
+)
 
 class Foreground extends React.Component {
 	componentDidMount() {
@@ -10,17 +29,12 @@ class Foreground extends React.Component {
 	render() {
 		return (
 			<div className="Foreground">
-			  <div className="header">
-			  <p> Score: 0 </p>
-			  </div>
-			  <div className="footer">
-			  <button>
-			{"<"}
-			  </button>
-			  <button>
-			{">"}
-			  </button>
-			  </div>
+			<Header as="h1" textAlign="center">
+			Score: 0 
+			</Header>
+			<div className="footer">
+			{buttons()}
+			</div>
 			</div>
 		)
 	}
